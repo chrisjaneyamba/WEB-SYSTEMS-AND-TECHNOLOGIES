25b3af3d6de7c2e50a92e44e3bae1d0cfaa8877d
chrisjaneyamba <80240145+chrisjaneyamba@users.noreply.github.com>
2022-06-27 22:50:50 +0800

    Initial commit

---
+++ Yamba_App/resources/views/login.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is Login page!
+    <a href = "{{ url('welcome')}}">Go to welcome Page</a>
+
+    
+
+    <h2>Login</h2>
+    <form method="POST" action="/login">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+
+        <div class="form-group">
+            <button style="cursor:pointer" type="submit" class="btn btn-primary">Login</button>
+        </div>
+        
+    </form>
+
+
+ 
+ 
+
+</body>
+</html>
